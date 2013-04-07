bf5a23b5f8977e14263433e468eb1cf5ee70e3f2
Adam Moon <adamjmoon@gmail.com>
2013-04-07 03:08:59 -0500

    fix test.js

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 define("Test", [], function() {
-  return function(shouldEqual, shouldBeA, expression) {
+  return function(shouldEqual, expression) {
     var expressionStr;
     expressionStr = expression.toString();
     this.expression = expressionStr.substring(25, expressionStr.length - 3);
     this.typeOf = typeof (expression());
     this.actual = expression();
     this.shouldEqual = shouldEqual;
-    this.shouldBeA = shouldBeA;
   };
 });
