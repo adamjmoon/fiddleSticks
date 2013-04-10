db95c7f75eb708546cfe67f998de1ff52f405686
Adam Moon <itchcork@gmail.com>
2013-04-09 23:44:13 -0500

    Update fiddleSticks.js

--- fiddleSticks.js
+++ fiddleSticks.js
@@ -10,7 +10,7 @@ define("Suite", ['Test', 'benchmark'], function(Test, Benchmark) {
 	self.benchmarkSuite = new Benchmark.Suite;
 	self.benchmarkPlatform = new ko.observable(self.benchmarkSuite.Platform);
  	self.benchmarkSuite.on('cycle', function(event) {
- 	  self.benchmarks[self.benchmarks.length](event.target);
+ 	  self.benchmarks[self.benchmarks.length] = event.target;
 	})
 	.on('complete', function() {
 //	  console.log('Fastest is ' + _.pluck(this.filter('fastest'), 'name'));
