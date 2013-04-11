b70a8f44f62049f761a5481206d90d087593a13c
Adam Moon <itchcork@gmail.com>
2013-04-11 10:40:22 -0500

    Update fiddleSticks.js

--- fiddleSticks.js
+++ fiddleSticks.js
@@ -32,10 +32,10 @@ define("Suite", ['Test', 'benchmark'], function(Test, Benchmark) {
 	   
 	   fastestBenchmark.timesFaster = timesFaster(fastestBenchmark.hz, slowestHz);
 	   self.benchmarks.push(fastestBenchmark);	   
-	   for (var i = 0; i < benchmarksCopy.length; i++) {
+	   /*for (var i = 0; i < benchmarksCopy.length; i++) {
 	        benchmarksCopy[i].timesFaster = timesFaster(benchmarksCopy[i].hz, slowestHz);
 	        self.benchmarks.push(benchmarksCopy[i]); 
-	   }	   
+	   }*/	   
 	   self.benchmarks.push(slowestBenchmark);
 	});
 	
