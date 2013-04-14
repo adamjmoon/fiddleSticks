a7b894d2f7ec98dca5439ad6252f4a8032dff667
Adam Moon <itchcork@gmail.com>
2013-04-14 11:39:00 -0500

    Update fiddleSticks.js

--- fiddleSticks.js
+++ fiddleSticks.js
@@ -113,11 +113,11 @@ define("Verify", [], function() {
 });
 
 define("FiddleSticks", ['Suite', 'Test', 'Spy', 'Verify'], function(Suite, Test, Spy, Verify) {
-  function FiddleSticks() {};
-  FiddleSticks.prototype.Suite = Suite;
-  FiddleSticks.prototype.Test = Test;
-  FiddleSticks.prototype.Spy = Spy;
-  FiddleSticks.prototype.Verify = Verify;
-  return FiddleSticks;
+  return function FiddleSticks() {
+	  FiddleSticks.prototype.Suite = Suite;
+	  FiddleSticks.prototype.Test = Test;
+	  FiddleSticks.prototype.Spy = Spy;
+	  FiddleSticks.prototype.Verify = Verify;
+  };
 });
 
