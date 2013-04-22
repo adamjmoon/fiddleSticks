3d3f2f8d57da494648dff5a8847d8afe3d823fdb
Adam Moon <itchcork@gmail.com>
2013-04-22 14:17:39 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,27 @@
 define("Test", [], function() {
-  return function(shouldEqual, expression, context) {
-    var expressionStr = expression.toString();  	
-  	this.name = expression.name;
-  	this.expression = expressionStr.substring(23, expressionStr.length - 2);
-  	this.shouldEqual = shouldEqual;
-  	this.actual = expression(context);
-  	this.typeOf = typeof(this.actual);
+  return function(shouldEqual, func, context, testCaseName) {
+    
+  	var expressionStr = func.toString().trim();  
+  	
+  	if(testCaseName){  		
+  		this.name = testCaseName;
+		this.expression =  expressionStr.replace(/\n    /,'')
+  				  	       	.replace(/{ return/,'{return')
+  				                .replace(/(function \(c, tc\)\{return c\[tc\])/gi,'context.' + testCaseName).replace(/\}/,'');
+	        this.actual = func(context,testCaseName);
+		
+  	} else{
+  		this.expression = expressionStr.replace(/\n    /,'')
+  				         .replace(/{ return/,'{return')
+  					 .replace(/function \(c\) {return /,'')
+  					 .replace(/c\./gi,'context.')  					 
+  					 .replace(/\}/,'');
+  		this.name = this.expression.replace(/context\./g,'')
+  					   .replace(/\;/,'');
+  		this.actual = func(context);
+  	}
+  	
+  	this.shouldEqual = shouldEqual;	
+	this.typeOf = typeof(this.actual);
   };
 });
