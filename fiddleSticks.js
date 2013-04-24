7326afa0b56a3be790f1f237c15f09d439e2297b
Adam Moon <itchcork@gmail.com>
2013-04-24 10:47:50 -0500

    Update fiddleSticks.js

--- fiddleSticks.js
+++ fiddleSticks.js
@@ -46,15 +46,16 @@ define("Suite", ['Test', 'benchmark'], function(Test, Benchmark) {
  	  self.benchmarks.push(event.target);
 	})
 	.on('complete', function() {
-           var slowestBenchmark = this.filter('slowest'),
-               fastestBenchmark = this.filter('fastest');
-	   if(slowestBenchmark && slowestBenchmark.length > 0){
+           var slowestBenchmark, fastestBenchmark,
+               slowestBenchmarkFilter = this.filter('slowest'),
+               fastestBenchmarkFilter = this.filter('fastest');
+	   if(slowestBenchmarkFilter && slowestBenchmarkFilter.length > 0){
 	   	 slowestBenchmark = this.filter('slowest')[0];
 		 slowestBenchmark.slowest = true;
 		 var slowestHz = slowestBenchmark.hz;
 		 self.benchmarks.remove(slowestBenchmark);
 	   }
-	   if(fastestBenchmark && fastestBenchmark.length > 0){
+	   if(fastestBenchmarkFilter && fastestBenchmarkFilter.length > 0){
 		   fastestBenchmark = this.filter('fastest')[0];	   
 		   fastestBenchmark.fastest = true;
 		   self.benchmarks.remove(fastestBenchmark);
