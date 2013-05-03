bf7fee26c1bc67d53a5558dd6a441a82684fc6b8
Adam Moon <adamjmoon@gmail.com>
2013-05-03 13:39:06 -0500

    fix regex

--- fiddleSticks.js
+++ fiddleSticks.js
@@ -201,7 +201,7 @@ define("ThemeManager", [], function() {
     		self.previousTheme = self.currentTheme;
 	    	self.currentTheme = newTheme;
 			var currentThemeStyle = document.getElementById(newTheme);
-			currentThemeStyle.innerHTML = currentThemeStyle.innerHTML.replace(/\/\*(.*?)\*\//g, "$1");
+			currentThemeStyle.innerHTML = currentThemeStyle.innerHTML.replace(/\/\*/g, "").replace(/\*\//g, "");
 			if(self.previousTheme != ''){
 				var previousThemeStyle = document.getElementById(self.previousTheme);
 				previousThemeStyle.innerHTML = '/*' + previousThemeStyle.innerHTML + '*/';
