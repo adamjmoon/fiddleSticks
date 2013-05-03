c36dc946b8136c50fc9e3d2fa6e2e9d9aeeb553b
Adam Moon <adamjmoon@gmail.com>
2013-05-03 18:06:49 -0500

    dsfdjskf

--- hitchscript/hitchscript.js
+++ hitchscript/hitchscript.js
@@ -1,47 +1,17 @@
 if (Meteor.isClient) {
 
-  Template.hitchscript.created = function(){
-    // var cssNode=document.createElement("link");
-    // cssNode.type = 'text/css';
-    // cssNode.rel = 'stylesheet';
-    // cssNode.href = themeFile;
-    // cssNode.media = 'all';
-    // cssNode.disabled = false;
-    // cssNode.targe = '_parent';
-    // var head = document.getElementsByTagName("head")[0];
-    // head.insertBefore(cssNode,head.firstChild);
-    
-    
-  };
-
-  Template.hitchscript.rendered = function() {
+  Template.hitchscript.created = function() {
 
     requirejs.config({
       baseUrl: 'https://',
       paths: {  
-        'FiddleSticks' : 'raw.github.com/adamjmoon/fiddleSticks/master/fiddleSticks',
-        'ThemeManager' : 'raw.github.com/adamjmoon/fiddleSticks/master/fiddleSticks',
+        'FiddleSticks' : 'raw.github.com/adamjmoon/fiddleSticks/master/fiddleSticks.min',
+        'ThemeManager' : 'raw.github.com/adamjmoon/fiddleSticks/master/fiddleSticks.min',
         'lodash' : 'cdnjs.cloudflare.com/ajax/libs/lodash.js/1.2.0/lodash.min',
         'platform' : 'cdnjs.cloudflare.com/ajax/libs/platform/0.4.0/platform.min',
         'benchmark' : 'raw.github.com/bestiejs/benchmark.js/master/benchmark',
         'knockout' : 'ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1',
-      }//,
-      // config: {
-      //     text: {
-      //         useXhr: function (url, protocol, hostname, port) {
-      //             //Override function for determining if XHR should be used.
-      //             //url: the URL being requested
-      //             //protocol: protocol of page text.js is running on
-      //             //hostname: hostname of page text.js is running on
-      //             //port: port of page text.js is running on
-      //             //Use protocol, hostname, and port to compare against the url
-      //             //being requested.
-      //             //Return true or false. true means "use xhr", false means
-      //             //"fetch the .js version of this resource".
-      //             return false;
-      //         }
-      //     }
-      // }
+      }
     });
     
     define("context", function() {
@@ -69,15 +39,13 @@ if (Meteor.isClient) {
     });
 
     require(['FiddleSticks','context'], function(fs, context) {
-      
        var fs = new fs();
        var suite = new fs.Suite('DateTime tests', context); 
-       suite.shouldEqual(1).compare(function(c, tc){return c[tc]();}).run();
+       suite.shouldEqual(1)
+            .compare(function(c, tc){return c[tc]();})
+            .run();
       
     });
-
-    
-    
   }
 };
 
