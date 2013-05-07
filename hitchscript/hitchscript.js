fed9b157a73b65a256f3aa5d8b3b46651bbfb8ec
Adam Moon <amoon@onetechnologies.net>
2013-05-07 17:30:36 -0500

    moved testcase dependency to github

--- hitchscript/hitchscript.js
+++ hitchscript/hitchscript.js
@@ -2,6 +2,12 @@ if (Meteor.isClient) {
 
   Template.hitchscript.created = function() {
 
+      var testcase='';
+          if(window.location.hash)
+              testcase = window.location.hash.split('#')[1];
+          else
+              testcase = 'datetime';
+
     requirejs.config({
       baseUrl: 'https://',
       paths: {  
@@ -10,13 +16,16 @@ if (Meteor.isClient) {
         'lodash' : 'cdnjs.cloudflare.com/ajax/libs/lodash.js/1.2.0/lodash.min',
         'platform' : 'cdnjs.cloudflare.com/ajax/libs/platform/0.4.0/platform.min',
         'benchmark' : 'raw.github.com/bestiejs/benchmark.js/master/benchmark',
-        'knockout' : 'ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1'
+        'knockout' : 'ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1',
+        'context' : 'raw.github.com/adamjmoon/fiddleSticks/master/testcases/' + testcase
       }
     });
-    
+
+
 
     require(['FiddleSticks','context'], function(fs, context) {
        var fs = new fs();
+       console.log(context.toString());
        var suite = new fs.Suite('DateTime tests', context); 
        suite.shouldEqual(1)
             .compare(function(c, tc){return c[tc]();})
